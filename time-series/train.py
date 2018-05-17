986827d2ceecaf9cc28b823d1e04ae827b18712a
Victor I. Afolabi <javafolabi@gmail.com>
2018-05-17 13:03:16 +0100

    Overriding the forward method of nn.Module

--- time-series/train.py
+++ time-series/train.py
@@ -51,17 +51,28 @@ class Sequence(nn.Module):
         self.hidden_size = hidden_size
 
         # Network architecture.
-        self.lstm1 = nn.LSTMCell(input_size=1, hidden_size=hidden_size)
-        self.lstm2 = nn.LSTMCell(input_size=hidden_size, hidden_size=hidden_size)
-        self.linear = nn.Linear(hidden_size, 1)
+        self.lstm1 = nn.LSTMCell(input_size=1,
+                                 hidden_size=hidden_size)
+        self.lstm2 = nn.LSTMCell(input_size=hidden_size,
+                                 hidden_size=hidden_size)
+        self.linear = nn.Linear(in_features=hidden_size,
+                                out_features=1)
 
     def forward(self, inputs, future=0):
         input_size = inputs.size()
         outputs = []
 
-        h_t = torch.zeros(input_size, self.hidden_size, dtype=torch.double)
-        c_t = torch.zeros(input_size, self.hidden_size, dtype=torch.double)
+        # Hidden & cell state for 1st time step.
+        h_t1 = torch.zeros(input_size, self.hidden_size, dtype=torch.double)
+        c_t1 = torch.zeros(input_size, self.hidden_size, dtype=torch.double)
 
+        # Hidden & cell state for 2nd time step.
+        h_t2 = torch.zeros(input_size, self.hidden_size, dtype=torch.double)
+        c_t2 = torch.zeros(input_size, self.hidden_size, dtype=torch.double)
+
+        for t, input_t in enumerate(inputs):
+            h_t1, c_t1 = self.lstm1(input_t, (h_t1, c_t1))
+            h_t2, c_t2 = self.lstm1(h_t1, (h_t2, c_t2))
 
 if __name__ == '__main__':
     main()
